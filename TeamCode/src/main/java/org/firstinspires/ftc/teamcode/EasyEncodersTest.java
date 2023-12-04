1888ed503f9a4738e759e26e840a6e96941f9d69
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2023-12-03 18:48:23 -0600

    V0.9 Created

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyEncodersTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyEncodersTest.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-@Autonomous
+@Autonomous()
 public class EasyEncodersTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
